7f5569bd05fc7f6d31900f1a2bd42fa732924881
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2018-04-14 00:04:29 -0500

    add

--- practica1-3.R
+++ practica1-3.R
@@ -1,4 +1,30 @@
+install.packages("arrangements")
+require("arrangements")
 pob = c(13.9, 11.5, 16.7, 14.4, 14.6, 15.1)
+combinations(x=pob, k =3, replace=TRUE)
+
+p = permutations(x=pob, k =3, replace=TRUE)
+
+m = matrix(p, nrow = 216, ncol = 3)
+
+ncol(m)
+nrow()
+
+length(m[,1])
+
+media = (matrix(m[,1]) + matrix(m[,2]) +matrix(m[,3]))/3
+
+cbind(m, media[,1]) 
+
+
+nrow((m[,1]))
+
+ 
+
+media <- m$0 + m$1  + m$2  
+
+
+choose(6,3)
 
 mean(pob)
 
@@ -13,14 +39,7 @@ sample(pob, 3, replace=TRUE)
 
 
 
-help(choose)
 
-devtools::install_github("randy3k/arrangements")
-
-
-install.packages("arrangements")
-require("arrangements")
-combinations(6, 3, pob)
 
 
 #ncol(combn(6,3))
