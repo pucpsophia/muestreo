2b5113644e9cec4318b322a98cc08ffd566ca631
gregory valderrama <gvalderrama@gregorys-iMac.local>
2018-04-19 15:18:36 -0500

    add

--- practica1-25.R
+++ practica1-25.R
@@ -3,66 +3,92 @@ install.packages("combinat")
 library("combinat")
 require("arrangements")
 options(digits = 5)
+getwd()
 
-movies <- read.csv(file="F:/muestreo/movies.csv", header=TRUE, sep=",")
-
+movie_path <- file.path(getwd(), "Documents", "studio" , "muestreo", "movies.csv")
+movies <- read.csv(file=movie_path, header=TRUE, sep=",")
 head(movies)
 
-indexes <-sample(x=nrow(movies),size=10,replace=FALSE)
+indexes <-sample(x=nrow(movies),size=12,replace=FALSE)
 
 sample_movie =  movies[indexes, ]
 
-sample_mean =as.numeric( apply(sample_movie[ , 3:12],1, sd))
+mean_sample_function <- function(vec){
+    d <- rep(c(1:10), vec)
+    m <- mean(d)
+    s2 <- (1/length(d)) * sum ( (d - m) **2)
+    res <- sqrt(s2)
+    return ( res )
+}
 
-hist(sample_mean)
+mean_sample_function2 <- function(vec){
+    d <- rep(c(1:10), vec)
+    s2 = ((length(d)-1)/length(d))*var(d)
+    res <- sqrt(s2)
+    return ( res )
+}
 
-pob_var = 0.27
+sample_sd <- apply(sample_movie[, 3:12],1, mean_sample_function)
 
-# muestra 
+# sample_var = ((length(sample_vars)-1)/length(sample_vars))*var(sample_vars)
 
-#a) Hallar el tamaño de la muestra
-e=0.01
-z=1.96
+sample_mean <- mean(sample_sd)
+sample_var <-  (1/length(sample_sd)) * sum ( (sample_sd - sample_mean) **2)
+sample_sd <- sqrt(sample_var)
 
-n<-((z**2) * (pob_var)*250)/( (z**2)*(pob_var) + ((e**2)*250))
+N <- 250
+e <- 0.1
+alpha = 0.05
+z= qnorm(1-alpha/2)
+
+n <- ( z ^ 2 * ( sample_var ) * N)/( z ^ 2 * ( sample_var )+( e ^ 2 ) * N )
+
 print(n)
 
-#muestra aleatoria
 
+# ================= Part B ===============================
 
-#sobre las puntuaciones de cada película
-p1<-rep(c(1:10),c(3127,463,413,502,998,2110,7538,17547,22598,36493))
+# n = 14 
+indexes <-sample(x=nrow(movies), size=14, replace=FALSE)
 
-hist(p1)
+sample_movie =  movies[indexes, ]
 
-p2<-rep(c(1:10),c(12307,4323,6004,1085,21228,53434,144929,300423,356058,389870))
-p3<-rep(c(1:10),c(2593,769,1067,1778,4232,12031,37473,74680,62964,60119))
-p4<-rep(c(1:10),c(15830,5649,7116,10665,21850,49119,140039,324975,511533,610427))
-p5<-rep(c(1:10),c(2294,882,1165,1806,4366,11364,32032,56447,46498,47029))
-p6<-rep(c(1:10),c(1648,368,335,425,856,1708,5276,9913,11990,24896))
-p7<-rep(c(1:10),c(4986,1896,2555,4275,10271,29092,90005,175112,134488,79853))
-p8<-rep(c(1:10),c(7406,1538,1785,2139,4198,8316,22499,47407,69776,103819))
-p9<-rep(c(1:10),c(2481,627,787,1117,2424,4665,12441,23059,21916,22324))
-p10<-rep(c(1:10),c(4972,1723,2326,4297,10479,28893,97892,206195,208905,187649))
-p11<-rep(c(1:10),c(5553,3282,4888,8281,17241,38787,85987,140494,131566,143494))
-p12<-rep(c(1:10),c(1795,551,614,1009,2407,6122,18944,34857,26936,24081))
+head(sample_movie)
 
-#Cálculo desviación estándar de cada película de la muestra del piloto
-#varianza poblacional
-varp1<-c((length(p1)-1)/length(p1)*var(p1),(length(p2)-1)/length(p2)*var(p2),(length(p3)-1)/length(p3)*var(p3),(length(p4)-1)/length(p4)*var(p4),(length(p5)-1)/length(p5)*var(p5),(length(p6)-1)/length(p6)*var(p6),(length(p7)-1)/length(p7)*var(p7),(length(p8)-1)/length(p8)*var(p8),(length(p9)-1)/length(p9)*var(p9),(length(p10)-1)/length(p10)*var(p10),(length(p11)-1)/length(p11)*var(p11),(length(p12)-1)/length(p12)*var(p12))
+sample_sd <- apply(sample_movie[, 3:12],1, mean_sample_function)
 
-print(varp1)
+sample_mean <- mean(sample_sd)
 
-#Varianza del piloto
-sd_p<-sqrt(varp1)
-print(sd_p)
+sample_var <-  (1/length(sample_sd)) * sum ( (sample_sd - sample_mean) **2)
 
-varf<-c((length(sd_p)-1)/length(sd_p)*var(sd_p))
-print(varf)  
+N <- 250
+n <- 14
+alpha = 0.05
+z= qnorm(1-alpha/2)
+e = 0.1
 
+error  <- ( z * sqrt(sample_var)/ sqrt(n) ) * sqrt( 1 - n/N )
 
-#Calcular tamaño de la muestra (n)
-n<-(z^2*(varf)*250)/(z^2*(varf)+(e^2)*250)
-print(n)
+lim_inf <- sample_mean - error
+
+lim_sup <- sample_mean + error
+
+result <- c( lim_inf, sample_mean, lim_sup)
+
+print(result)
+
+# ================= Part C ===============================
+
+whiplash<-rep(c(1:10), as.numeric(movies[movies$name == 'Whiplash', 3:12]))
+#  2989 1410 2029 3244 6710 18499 63151 148482 177511 120836
+
+e_mean = mean(whiplash)
+
+e_var = (1/length(whiplash)) * sum ( (whiplash - e_mean) **2)
+
+e_sd = sqrt(e_var)
+
+print(e_sd)
+
 
 
