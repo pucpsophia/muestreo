72c7c241883db7c9bfc8916d030132ae15b22a75
gregory valderrama <gvalderrama@gregorys-iMac.local>
2018-04-20 17:32:26 -0500

    add

--- practica1-28.R
+++ practica1-28.R
@@ -4,3 +4,22 @@ library("combinat")
 require("arrangements")
 options(digits = 5)
 
+
+# previous 
+
+
+
+
+# 1600 urnas seleccionadas de una gran poblacion 
+# 812 votaron por el candidato opositor 
+# 480 votaron por el candidato estatal 
+# 50 votaron en blanco
+# 258 votaron invalido
+
+alpha = 0.05
+
+N <- dim(apipop)[1]
+n <- 500
+
+
+load("TallaS.RData")
