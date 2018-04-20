eb0826986455306933e2e96573aca057487f0357
Gregory Valderrama <gcvalderrama@hotmail.com>
2018-04-20 00:31:40 -0500

    add

--- practica1-25.R
+++ practica1-25.R
@@ -5,7 +5,8 @@ require("arrangements")
 options(digits = 5)
 getwd()
 
-movie_path <- file.path(getwd(), "Documents", "studio" , "muestreo", "movies.csv")
+movie_path <- file.path(getwd(), "Documents", "pucp" , "muestreo", "movies.csv")
+
 movies <- read.csv(file=movie_path, header=TRUE, sep=",")
 head(movies)
 
@@ -13,27 +14,20 @@ indexes <-sample(x=nrow(movies),size=12,replace=FALSE)
 
 sample_movie =  movies[indexes, ]
 
-mean_sample_function <- function(vec){
+sample_movie
+
+sample_function <- function(vec){
     d <- rep(c(1:10), vec)
     m <- mean(d)
-    s2 <- (1/length(d)) * sum ( (d - m) **2)
+    s2 <- (1/ (length(d) -1) ) * sum ( (d - m) **2)
     res <- sqrt(s2)
     return ( res )
 }
 
-mean_sample_function2 <- function(vec){
-    d <- rep(c(1:10), vec)
-    s2 = ((length(d)-1)/length(d))*var(d)
-    res <- sqrt(s2)
-    return ( res )
-}
+sample_sd_vec <- apply(sample_movie[, 3:12],1, sample_function)
 
-sample_sd <- apply(sample_movie[, 3:12],1, mean_sample_function)
-
-# sample_var = ((length(sample_vars)-1)/length(sample_vars))*var(sample_vars)
-
-sample_mean <- mean(sample_sd)
-sample_var <-  (1/length(sample_sd)) * sum ( (sample_sd - sample_mean) **2)
+sample_mean <- mean(sample_sd_vec)
+sample_var <-  (1/length(sample_sd_vec)) * sum ( (sample_sd_vec - sample_mean) **2)
 sample_sd <- sqrt(sample_var)
 
 N <- 250
@@ -41,25 +35,13 @@ e <- 0.1
 alpha = 0.05
 z= qnorm(1-alpha/2)
 
-n <- ( z ^ 2 * ( sample_var ) * N)/( z ^ 2 * ( sample_var )+( e ^ 2 ) * N )
+# duda cual es esta formula 
+n <- ( z ^ 2 * ( sample_var ) * N)/( z ^ 2 * ( sample_var ) + ( e ^ 2 ) * N )
 
 print(n)
 
 
 # ================= Part B ===============================
-
-# n = 14 
-indexes <-sample(x=nrow(movies), size=14, replace=FALSE)
-
-sample_movie =  movies[indexes, ]
-
-head(sample_movie)
-
-sample_sd <- apply(sample_movie[, 3:12],1, mean_sample_function)
-
-sample_mean <- mean(sample_sd)
-
-sample_var <-  (1/length(sample_sd)) * sum ( (sample_sd - sample_mean) **2)
 
 N <- 250
 n <- 14
@@ -70,12 +52,12 @@ e = 0.1
 error  <- ( z * sqrt(sample_var)/ sqrt(n) ) * sqrt( 1 - n/N )
 
 lim_inf <- sample_mean - error
-
 lim_sup <- sample_mean + error
 
-result <- c( lim_inf, sample_mean, lim_sup)
+print(paste0( "mean ", sample_mean))
+print(paste0( "lim inf ", lim_inf))
+print(paste0( "lim sup ", lim_sup))
 
-print(result)
 
 # ================= Part C ===============================
 
@@ -91,4 +73,16 @@ e_sd = sqrt(e_var)
 print(e_sd)
 
 
+
+# sample_var = ((length(sample_vars)-1)/length(sample_vars))*var(sample_vars)
+
+
+# n-1/n S2
+
+mean_sample_function2 <- function(vec){
+  d <- rep(c(1:10), vec)
+  s2 = ((length(d)-1)/length(d))*var(d)
+  res <- sqrt(s2)
+  return ( res )
+}
 
--- practica1-9.R
+++ practica1-9.R
@@ -2,20 +2,32 @@ install.packages("arrangements")
 install.packages("combinat")
 library("combinat")
 require("arrangements")
+
+install.packages("rootSolve")
+library(rootSolve)
 options(digits = 5)
 
 
-N=500
-n=30
+N = 500
+n = 30
 x = 2
 alpha = 0.05
 pbar = x/n
-z= qnorm(1-alpha/2)
-e=0.03
+z = qnorm(1-alpha/2)
+e = 0.03
 
-wald= ((z^2)*pbar*(1-pbar))/(e^2)
+wald = ( ( z ^ 2 * pbar * ( 1 - pbar ) ) * N )/( ( z * pbar * ( 1 - pbar ) ) + ( e ^ 2 * ( N - 1 )))
 wald
 
-wilson =((z^2*pbar*(1-pbar))*N)/((z*pbar*(1-pbar))+(e^2*(N-1)))
-wilson
 
+fun_wilson <- function (n) {
+  4 * ( (z ^ 2) * (N - n) / (n * (N -1) ) ) + ((z ^ 2) * (N - n) / (n * (N -1) )) ^ 2 - 4 * ((z ^ 2) * (N - n) / (n * (N -1) )) * ( pbar ^ 2) - e  
+} 
+
+uniroot.all(fun_wilson, c(0, 500))
+
+
+fun_wald <- function (n) (((((z ^ 2) * (N - n)) / (n * (N - 1))) ^ (1 / 2)) * ( - (4 * (p ^ 2)) + (4 * p)) ^ (1 / 2)) / 2 - e
+
+
+uniroot.all(fun_wald, c(0, 500))
