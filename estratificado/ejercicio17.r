69f01ddd07bbadd20e4bfb883cac789888e3303a
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2018-05-27 23:34:27 -0500

    add

--- estratificado/ejercicio17.r
+++ estratificado/ejercicio17.r
@@ -27,31 +27,27 @@ movies[c(which(movies$year  >= 1980  & movies$year < 1990)), 'estrato'] <- 3
 movies[c(which(movies$year  >= 1990)), 'estrato'] <- 4
 
 
-movies_d <- cbind( movies, mean = c(apply(movies[,3:12] , 1, mean_function)))
+movies_d <- cbind(movies, mean = c(apply(movies[,3:12] , 1, mean_function)))
 
 str(movies_d)
 
-
 unique(movies$estrato)
 
-movies[sample(which(movies$estrato == 1), 10), ]
 
 
-means = apply(array, margin, ...)
-movies_d[55, ]
-(movies [55, "v1"] * 1 + movies [55, "v2"] * 2 + movies [55, "v3"] * 3 + movies [55, "v4"] * 4 +movies [55, "v5"] * 5 + movies [55, "v6"] * 6 + movies [55, "v7"] * 7 +movies [55, "v8"] * 8 + movies [55, "v9"] * 9 + movies [55, "v10"] * 10) / sum(movies[55, 3:12])
-
-movies [55, ]
 # muestra piloto 
 
-piloto <-sample(x=nrow(movies),size=10,replace=FALSE)
-sample_movie_test =  movies[piloto, ]
+piloto_estrato_1 <- movies_d[sample(which(movies_d$estrato == 1), 10), ]
+piloto_estrato_2 <- movies_d[sample(which(movies_d$estrato == 2), 10), ]
+piloto_estrato_3 <- movies_d[sample(which(movies_d$estrato == 3), 10), ]
+piloto_estrato_4 <- movies_d[sample(which(movies_d$estrato == 4), 10), ]
 
 
-sigma_h_0070 = sd(Pop$M500_M[Pop$Estrato=="Urbana.Estatal"][sample(Nh[1],10)])
-sigma_h_7080 = sd(Pop$M500_M[Pop$Estrato=="Urbana.Estatal"][sample(Nh[1],10)])
-sigma_h_8090 = sd(Pop$M500_M[Pop$Estrato=="Urbana.Estatal"][sample(Nh[1],10)])
-sigma_h_9000 = sd(Pop$M500_M[Pop$Estrato=="Urbana.Estatal"][sample(Nh[1],10)])
+sigma_h_estrato_1 = sd(piloto_estrato_1$mean)
+sigma_h_estrato_2 = sd(piloto_estrato_2$mean)
+sigma_h_estrato_3 = sd(piloto_estrato_3$mean)
+sigma_h_estrato_4 = sd(piloto_estrato_4$mean)
+
 
 
 ah = Nh*sigmah/sum(Nh*sigmah)
