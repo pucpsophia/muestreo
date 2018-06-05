c453c77ce9d476cb9cc9f6f1d0578b468535a276
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2018-06-04 23:48:25 -0500

    add

--- practica2/ejercicio17.r
+++ practica2/ejercicio17.r
@@ -8,39 +8,21 @@ library(survey)
 library(sampling)
 library("combinat")
 require("arrangements")
+
 options(digits = 5)
 setwd("f:/muestreo")
 movies <- read.csv(file= "movies.csv", header=TRUE, sep=",")
 head(movies)
-
 str(movies)
-
 nrow(movies)
-
 str(d)
 
 
-# pregunta 25 anterior 
-# La Internet Movie Database (IMDb) es una base de datos en lnea que almacena informacion relacionada con peliculas, 
-# personal de equipo de produccion (incluyendo directores y productores), actores, series de television, programas de television, videojuegos, actores de
-# doblaje y, mas recientemente, personajes cticios que aparecen en los medios de entretenimiento
-# visual. Recibe mas de 100 millones de usuarios unicos al mes y cuenta con una version
-# movil. Una de sus secciones, \The IMDb Top 250" es destinada a ser un listado de las 250
-# pelculas con mejor calicacion, basado en calicaciones de los usuarios registrados del sitio
-# web. En esta seccion cada pelcula aparece con una estrella y un ranking de a lo mas 10 puntos.
-# Debajo de este ranking uno puede acceder a las calicaciones otorgadas por los usuarios
-# en forma de un histograma. La intencion de este miniproyecto es estimar, con un margen de
-# error de a lo mas 0.1 puntos y un nivel de conanza del 95 %, la desviacion estandar media
-# (como medida de controversia) de los rankings asignados a estas 250 pelculas.
+movies[c(which(movies$year  < 1970)), 'estrato'] <- 1
+movies[c(which(movies$year  >= 1970  & movies$year < 1980)), 'estrato'] <- 2
+movies[c(which(movies$year  >= 1980  & movies$year < 1990)), 'estrato'] <- 3
+movies[c(which(movies$year  >= 1990)), 'estrato'] <- 4
 
-
-# pregunta 17
-# Considere el ejercicio 26 del captulo anterior y replique este estudio, pero ahora utilizando
-# un MAE con asignacion de Neyman, donde su variable de estraticacion sera el a~no
-# de la pelcula. Concretamente considere en un primer estrato a aquellas pelculas que sean
-# anteriores a 1970, otro estrato con pelculas entre los 70 y anteriores a los 80, un tercer
-# estrato con pelculas de entre los 80 y anteriores a los 90 y un estrato nal con pelculas de
-# los 90 hasta la actualidad.
 
 s_function <- function(vec){
   d <- rep(c(1:10), vec)
@@ -50,89 +32,87 @@ s_function <- function(vec){
   return ( res )
 }
 
-
-movies[c(which(movies$year  < 1970)), 'estrato'] <- 1
-movies[c(which(movies$year  >= 1970  & movies$year < 1980)), 'estrato'] <- 2
-movies[c(which(movies$year  >= 1980  & movies$year < 1990)), 'estrato'] <- 3
-movies[c(which(movies$year  >= 1990)), 'estrato'] <- 4
-
-
+# generate the standard deviation for movies based on opinions 
 movies_d <- cbind(movies, sd = c(apply(movies[,3:12] , 1, s_function)))
-
 str(movies_d)
-
-
-movies_d = movies_d[order(movies_d$estrato),]
 table(movies_d$estrato)
+# 1   2   3   4 
+# 68  20  30 132 
 
-# muestra piloto 
-
-piloto_estrato_1 <- movies_d[sample(which(movies_d$estrato == 1), 10), ]
-piloto_estrato_2 <- movies_d[sample(which(movies_d$estrato == 2), 10), ]
-piloto_estrato_3 <- movies_d[sample(which(movies_d$estrato == 3), 10), ]
-piloto_estrato_4 <- movies_d[sample(which(movies_d$estrato == 4), 10), ]
+# muestra piloto
 
 
+piloto_estrato = vector(length = 4)
+# piloto_estrato_1 <- movies_d[sample(which(movies_d$estrato == 1), 12), ]
+# selected values by movie Id
+piloto_estrato[1] = movies_d[is.element(movies_d$id, c(31381,19254,50986,59578,33870,33467,55031,15324,50976,21749,41959,53125)), ]
+# piloto_estrato_2 <- movies_d[sample(which(movies_d$estrato == 2), 12), ]
+piloto_estrato[2] = movies_d[is.element(movies_d$id, c(75314,73195,71853,75686,75148,66921,73486,79470,79944,70735,70510,77416)), ]
+# piloto_estrato_3 <- movies_d[sample(which(movies_d$estrato == 3), 12), ]
+piloto_estrato[3] = movies_d[is.element(movies_d$id, c(86250,93058,95327,81505,88247,86879,82096,83658,87544,80678,87843,88763)), ]
+# piloto_estrato_4 <- movies_d[sample(which(movies_d$estrato == 4), 12), ]
+piloto_estrato[4] = movies_d[is.element(movies_d$id, c(5074352,338013,2106476,1675434,266543,3783958,2267998,245712,198781,120586,1832382,986264)), ]
 
+Nh_estrato = vector(length = 4)
+Nh_estrato[1]  = nrow(movies_d[which(movies_d$estrato == 1),])
+Nh_estrato[2]  = nrow(movies_d[which(movies_d$estrato == 2),])
+Nh_estrato[3]  = nrow(movies_d[which(movies_d$estrato == 3),])
+Nh_estrato[4]  = nrow(movies_d[which(movies_d$estrato == 4),])
 
+# validation 
+stopifnot( sum(Nh_estrato) == 250)
 
-Nh_estrato_1  = nrow(movies_d[which(movies_d$estrato == 1),])
-Nh_estrato_2  = nrow(movies_d[which(movies_d$estrato == 2),])
-Nh_estrato_3  = nrow(movies_d[which(movies_d$estrato == 3),])
-Nh_estrato_4  = nrow(movies_d[which(movies_d$estrato == 4),])
+sigma_h = vector(length = 4)
+sigma_h[1] = sd(piloto_estrato_1$sd)
+sigma_h[2] = sd(piloto_estrato_2$sd)
+sigma_h[3] = sd(piloto_estrato_3$sd)
+sigma_h[4] = sd(piloto_estrato_4$sd)
 
-stopifnot( sum(Nh_estrato_1,  Nh_estrato_2, Nh_estrato_3, Nh_estrato_4) == 250)
+sum_Nh = sum(Nh_estrato * sigma_h) 
 
-sigma_h_estrato_1 = sd(piloto_estrato_1$sd)
-sigma_h_estrato_2 = sd(piloto_estrato_2$sd)
-sigma_h_estrato_3 = sd(piloto_estrato_3$sd)
-sigma_h_estrato_4 = sd(piloto_estrato_4$sd)
-
-
-sum_Nh = (Nh_estrato_1 * sigma_h_estrato_1) +  (Nh_estrato_2 * sigma_h_estrato_2) + (Nh_estrato_3 * sigma_h_estrato_3) + (Nh_estrato_4 * sigma_h_estrato_4)
-
-ah_1 = ( Nh_estrato_1 * sigma_h_estrato_1)/sum_Nh
-ah_2 = ( Nh_estrato_2 * sigma_h_estrato_2)/sum_Nh
-ah_3 = ( Nh_estrato_3 * sigma_h_estrato_3)/sum_Nh
-ah_4 = ( Nh_estrato_4 * sigma_h_estrato_4)/sum_Nh
-
+ah = (Nh_estrato * sigma_h)/sum_Nh
 
 N  = 250
 e = 0.1
 alpha = 0.05
 z= qnorm(1-alpha/2)
-
 d = N*e/z
 
-sum_n_a =  ( Nh_estrato_1 * sigma_h_estrato_1 ) ^ 2 / ah_1 + ( Nh_estrato_2 * sigma_h_estrato_2 ) ^ 2 / ah_2 + ( Nh_estrato_3 * sigma_h_estrato_3 ) ^ 2 / ah_3 + ( Nh_estrato_4 * sigma_h_estrato_4 ) ^ 2 / ah_4
-sum_n =   ( Nh_estrato_1 * sigma_h_estrato_1 ^ 2) + ( Nh_estrato_2 * sigma_h_estrato_2 ^ 2)  + ( Nh_estrato_3 * sigma_h_estrato_3 ^ 2)  + ( Nh_estrato_4 * sigma_h_estrato_4 ^ 2)  
-n = sum_n_a/(d ^ 2 + sum_n)
+sum_n_a =  sum( ( Nh_estrato * sigma_h ) ^ 2 / ah )
+sum_n = sum( Nh_estrato * sigma_h ^ 2)
 
-nh_1 = ceiling(ah_1 * n)
-nh_2 = ceiling(ah_2 * n)
-nh_3 = ceiling(ah_3 * n)
-nh_4 = ceiling(ah_4 * n)
+n =  ceiling(sum_n_a/(d ^ 2 + sum_n)) #18
+nh = ceiling(ah * n) # 6 2 2 11 = 21
 
-# check if the value is 1 
-# nh_2 = 2
+# sort by estrato
+movies_d = movies_d[order(movies_d$estrato),]
 
-# http://r-survey.r-forge.r-project.org/survey/exmample-lonely.html
-options(survey.lonely.psu="adjust")
+m <- sampling::strata(movies_d, c("estrato"), size= nh, method="srswor")
+sample_mae = getdata(movies_d,m)
 
-m <- sampling::strata(movies_d, c("estrato"), size= c(nh_1, nh_2, nh_3, nh_4), method="srswor")
-me16Am = getdata(movies_d,m)
+table(is.na(sample_mae$sd))
 
-table(is.na(me16Am$sd))
+aux = data.frame(fpc = c(rep(Nh_estrato[1],nh[1]),rep(Nh_estrato[2],nh[2]), rep(Nh_estrato[3],nh[3]), rep(Nh_estrato[4],nh[4])))
+sample_mae_fpc = cbind(num = 1 :sum(nh),sample_mae,aux)
 
-aux = data.frame(fpc = c(rep(Nh_estrato_1,nh_1),rep(Nh_estrato_2,nh_2), rep(Nh_estrato_3,nh_3), rep(Nh_estrato_4,nh_4)))
-sampleMAE = cbind(num = 1:( nh_1+ nh_2 + nh_3 + nh_4  ),me16Am,aux)
+design_mae = svydesign(id=~1,strata=~estrato,fpc=~fpc,data=sample_mae_fpc)
 
-dis16MAE = svydesign(id=~1,strata=~estrato,fpc=~fpc,data=sampleMAE)
-
-
-
-means1 = svymean(~sd,dis16MAE, deff=T)
+means1 = svymean(~sd,design_mae, deff=T)
 
 means1
+# mean     SE DEff
+# sd 1.5976 0.0353 0.56
+confint(means1)
+# 2.5 % 97.5 %
+# sd 1.5284 1.6667
 
+
+
+# ================= Part C ===============================
+
+whiplash<-rep(c(1:10), as.numeric(movies[movies$name == 'Whiplash', 3:12]))
+#  2989 1410 2029 3244 6710 18499 63151 148482 177511 120836
+e_mean = mean(whiplash)
+sd =  sd(whiplash)
+# 1.3882
 
