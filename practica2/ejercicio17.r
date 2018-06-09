ed4bdd541df6c47d6078ec8d740e1d11df498a94
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2018-06-08 22:28:49 -0500

    add

--- practica2/ejercicio17.r
+++ practica2/ejercicio17.r
@@ -7,7 +7,9 @@ install.packages("survey")
 library(survey)
 library(sampling)
 library("combinat")
-require("arrangements")
+library("arrangements")
+
+
 
 options(digits = 5)
 setwd("f:/muestreo")
@@ -42,16 +44,16 @@ table(movies_d$estrato)
 # muestra piloto
 
 
-piloto_estrato = vector(length = 4)
+
 # piloto_estrato_1 <- movies_d[sample(which(movies_d$estrato == 1), 12), ]
 # selected values by movie Id
-piloto_estrato[1] = movies_d[is.element(movies_d$id, c(31381,19254,50986,59578,33870,33467,55031,15324,50976,21749,41959,53125)), ]
+piloto_estrato_1 = movies_d[is.element(movies_d$id, c(31381,19254,50986,59578,33870,33467,55031,15324,50976,21749,41959,53125)), ]
 # piloto_estrato_2 <- movies_d[sample(which(movies_d$estrato == 2), 12), ]
-piloto_estrato[2] = movies_d[is.element(movies_d$id, c(75314,73195,71853,75686,75148,66921,73486,79470,79944,70735,70510,77416)), ]
+piloto_estrato_2 = movies_d[is.element(movies_d$id, c(75314,73195,71853,75686,75148,66921,73486,79470,79944,70735,70510,77416)), ]
 # piloto_estrato_3 <- movies_d[sample(which(movies_d$estrato == 3), 12), ]
-piloto_estrato[3] = movies_d[is.element(movies_d$id, c(86250,93058,95327,81505,88247,86879,82096,83658,87544,80678,87843,88763)), ]
+piloto_estrato_3 = movies_d[is.element(movies_d$id, c(86250,93058,95327,81505,88247,86879,82096,83658,87544,80678,87843,88763)), ]
 # piloto_estrato_4 <- movies_d[sample(which(movies_d$estrato == 4), 12), ]
-piloto_estrato[4] = movies_d[is.element(movies_d$id, c(5074352,338013,2106476,1675434,266543,3783958,2267998,245712,198781,120586,1832382,986264)), ]
+piloto_estrato_4 = movies_d[is.element(movies_d$id, c(5074352,338013,2106476,1675434,266543,3783958,2267998,245712,198781,120586,1832382,986264)), ]
 
 Nh_estrato = vector(length = 4)
 Nh_estrato[1]  = nrow(movies_d[which(movies_d$estrato == 1),])
@@ -93,7 +95,7 @@ sample_mae = getdata(movies_d,m)
 table(is.na(sample_mae$sd))
 
 aux = data.frame(fpc = c(rep(Nh_estrato[1],nh[1]),rep(Nh_estrato[2],nh[2]), rep(Nh_estrato[3],nh[3]), rep(Nh_estrato[4],nh[4])))
-sample_mae_fpc = cbind(num = 1 :sum(nh),sample_mae,aux)
+sample_mae_fpc = cbind(num = 1 :sum(nh) , sample_mae , aux )
 
 design_mae = svydesign(id=~1,strata=~estrato,fpc=~fpc,data=sample_mae_fpc)
 
