bea002a5990925b9753b3b69422d04a8e04616fd
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2018-07-06 00:14:09 -0500

    add

--- final/pregunta.r
+++ final/pregunta.r
@@ -24,6 +24,11 @@
 # decir, cuando vea que el numero de obras encuestadas es 0), impute esta proporcion simulando ella de una
 # distribucion Beta de parametros alpha = 2 y beta = 8. (2.0 puntos)
 
+
+setwd("F:/muestreo")
+setwd("/Users/gvalderrama/Documents/muestreo/")
+setwd("/Users/gregory/Documents/pucp/muestreo/")
+
 install.packages("data.table")
 install.packages("sampling")
 install.packages("survey")
@@ -34,12 +39,6 @@ library(survey)
 library(data.table)
 library(foreign)
 
-setwd("F:/muestreo")
-
-setwd("/Users/gvalderrama/Documents/muestreo/")
-
-setwd("/Users/gregory/Documents/pucp/muestreo/")
-
 dataset <- read.csv(file= "DATAEX.csv", header=TRUE, sep=";")
 
 head(dataset)
@@ -49,30 +48,21 @@ levels(dataset$ESTRATO)
 levels(dataset$DISTRITO)
 levels(dataset$SECTOR)
 
-
-
 datatable = as.data.table(dataset)
 datatable <- datatable[ order ( datatable$ESTRATO ) , ]
-
 
 table(datatable$ESTRATO)
 table(datatable$DISTRITO)
 table(datatable$SECTOR)
 
-
 datatable [ , NESTRATO := 0]
 datatable [ , NDISTRITO := 0]
 datatable [ , NSECTOR := 0]
-
-str(datatable)
 
 # check missing data 
 nads = na.omit(datatable)
 dim(datatable)
 dim(nads)
-
-
-
 # ================ TOTAL DE OBRAS POR ESTRATO =============================
 
 NESTRATO_LIMA_TOP <- 423  # total obras por estrato
@@ -90,9 +80,7 @@ datatable[which(datatable$ESTRATO == "LIMA NORTE", arr.ind=T), "NESTRATO" ] = NE
 datatable[which(datatable$ESTRATO == "LIMA SUR", arr.ind=T), "NESTRATO" ] = NESTRATO_LIMA_SUR
 datatable[which(datatable$ESTRATO == "CALLAO", arr.ind=T), "NESTRATO" ] = NESTRATO_CALLAO
 
-
 # ================ TOTAL DE OBRAS POR DISTRITO =============================
-
 # DISTRITO LIMA TOP
 datatable[which(datatable$ESTRATO == "LIMA TOP" & datatable$DISTRITO == "MIRAFLORES"), "NDISTRITO" ] = 133
 datatable[which(datatable$ESTRATO == "LIMA TOP" & datatable$DISTRITO == "SAN ISIDRO"), "NDISTRITO" ] = 58
@@ -100,54 +88,39 @@ datatable[which(datatable$ESTRATO == "LIMA TOP" & datatable$DISTRITO == "SAN ISI
 datatable[which(datatable$ESTRATO == "LIMA TOP" & datatable$DISTRITO == "SURCO"), "NDISTRITO" ] = 136
 datatable[which(datatable$ESTRATO == "LIMA TOP" & datatable$DISTRITO == "SAN BORJA"), "NDISTRITO" ] = 53
 datatable[which(datatable$ESTRATO == "LIMA TOP" & datatable$DISTRITO == "BARRANCO"), "NDISTRITO" ] = 34
-
 # DISTRITO LIMA MODERNA
-
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "JESUS MARIA"), "NDISTRITO" ] = 60
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "LINCE"), "NDISTRITO" ] = 54
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "MAGDALENA"), "NDISTRITO" ] = 64
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "PUEBLO LIBRE"), "NDISTRITO" ] = 53
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "SAN MIGUEL"), "NDISTRITO" ] = 82
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "SURQUILLO"), "NDISTRITO" ] = 47
-
 # DISTRITO LIMA CENTRO  
-
 datatable[which(datatable$ESTRATO == "LIMA CENTRO" & datatable$DISTRITO == "CERCADO LIMA"), "NDISTRITO" ] = 24
 datatable[which(datatable$ESTRATO == "LIMA CENTRO" & datatable$DISTRITO == "BRENIA"), "NDISTRITO" ] = 34
 datatable[which(datatable$ESTRATO == "LIMA CENTRO" & datatable$DISTRITO == "LA VICTORIA"), "NDISTRITO" ] = 37
 # san luis 0
-
 # DISTRITO LIMA ESTE 
-
 datatable[which(datatable$ESTRATO == "LIMA ESTE" & datatable$DISTRITO == "ATE"), "NDISTRITO" ] = 36
-# Cieneguilla
-# Chaclacayo
-# Lurigancho
+# Cieneguilla # Chaclacayo # Lurigancho
 datatable[which(datatable$ESTRATO == "LIMA ESTE" & datatable$DISTRITO == "SANTA ANITA"), "NDISTRITO" ] = 7
 datatable[which(datatable$ESTRATO == "LIMA ESTE" & datatable$DISTRITO == "EL AGUSTINO"), "NDISTRITO" ] = 3
 # San Juan de Lurigancho
-
 # DISTRITO NORTE 
-
 datatable[which(datatable$ESTRATO == "LIMA NORTE" & datatable$DISTRITO == "CARABAYLLO"), "NDISTRITO" ] = 23
 datatable[which(datatable$ESTRATO == "LIMA NORTE" & datatable$DISTRITO == "COMAS"), "NDISTRITO" ] = 42
 datatable[which(datatable$ESTRATO == "LIMA NORTE" & datatable$DISTRITO == "LOS OLIVOS"), "NDISTRITO" ] = 57
 # puente piedra
 datatable[which(datatable$ESTRATO == "LIMA NORTE" & datatable$DISTRITO == "SMP"), "NDISTRITO" ] = 6
 datatable[which(datatable$ESTRATO == "LIMA NORTE" & datatable$DISTRITO == "ANCON/INDEP"), "NDISTRITO" ] = 2
-
 # DISTRITO SUR 
-
 datatable[which(datatable$ESTRATO == "LIMA SUR" & datatable$DISTRITO == "CHORRILLOS"), "NDISTRITO" ] = 35
 datatable[which(datatable$ESTRATO == "LIMA SUR" & datatable$DISTRITO == "LURIN/PACH/VES"), "NDISTRITO" ] = 13
 datatable[which(datatable$ESTRATO == "LIMA SUR" & datatable$DISTRITO == "SAN JUAN MIRAFLORES"), "NDISTRITO" ] = 11
 #Pucusana
 datatable[which(datatable$ESTRATO == "LIMA SUR" & datatable$DISTRITO == "PUNTA HERMOSA/NEGRA"), "NDISTRITO" ] = 13
 datatable[which(datatable$ESTRATO == "LIMA SUR" & datatable$DISTRITO == "SAN BARTOLO"), "NDISTRITO" ] = 14
-
-# Santa María del Mar
-
-
+# Santa Maria del Mar
 # DISTRITO CALLAO
 datatable[which(datatable$ESTRATO == "CALLAO" & datatable$DISTRITO == "BELLAVISTA"), "NDISTRITO" ] = 10
 datatable[which(datatable$ESTRATO == "CALLAO" & datatable$DISTRITO == "CALLAO"), "NDISTRITO" ] = 14
@@ -156,111 +129,69 @@ datatable[which(datatable$ESTRATO == "CALLAO" & datatable$DISTRITO == "VENTANILL
 
 
 # ================ TOTAL DE OBRAS POR SECTOR =============================
-
-
 # DISTRITO LIMA TOP
-
 datatable[which(datatable$ESTRATO == "LIMA TOP" & datatable$DISTRITO == "MIRAFLORES" & datatable$SECTOR == "A" ), "NSECTOR" ] = 34
 datatable[which(datatable$ESTRATO == "LIMA TOP" & datatable$DISTRITO == "MIRAFLORES" & datatable$SECTOR == "B" ), "NSECTOR" ] = 77
 datatable[which(datatable$ESTRATO == "LIMA TOP" & datatable$DISTRITO == "MIRAFLORES" & datatable$SECTOR == "C" ), "NSECTOR" ] = 23
-
 datatable[which(datatable$ESTRATO == "LIMA TOP" & datatable$DISTRITO == "SAN ISIDRO" & datatable$SECTOR == "A" ), "NSECTOR" ] = 49
 datatable[which(datatable$ESTRATO == "LIMA TOP" & datatable$DISTRITO == "SAN ISIDRO" & datatable$SECTOR == "B" ), "NSECTOR" ] = 9
 #  molina 
 datatable[which(datatable$ESTRATO == "LIMA TOP" & datatable$DISTRITO == "SURCO" & datatable$SECTOR == "A" ), "NSECTOR" ] = 66
 datatable[which(datatable$ESTRATO == "LIMA TOP" & datatable$DISTRITO == "SURCO" & datatable$SECTOR == "B" ), "NSECTOR" ] = 50
 datatable[which(datatable$ESTRATO == "LIMA TOP" & datatable$DISTRITO == "SURCO" & datatable$SECTOR == "C" ), "NSECTOR" ] = 20
-
 datatable[which(datatable$ESTRATO == "LIMA TOP" & datatable$DISTRITO == "SAN BORJA" & datatable$SECTOR == "A" ), "NSECTOR" ] = 19
 datatable[which(datatable$ESTRATO == "LIMA TOP" & datatable$DISTRITO == "SAN BORJA" & datatable$SECTOR == "B" ), "NSECTOR" ] = 34
 datatable[which(datatable$ESTRATO == "LIMA TOP" & datatable$DISTRITO == "SAN BORJA" & datatable$SECTOR == "C" ), "NSECTOR" ] = 1
-
 datatable[which(datatable$ESTRATO == "LIMA TOP" & datatable$DISTRITO == "BARRANCO" & datatable$SECTOR == "A" ), "NSECTOR" ] = 34
-
-
 # DISTRITO LIMA MODERNA
-
-
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "JESUS MARIA" & datatable$SECTOR == "A" ), "NSECTOR" ] = 31
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "JESUS MARIA" & datatable$SECTOR == "B" ), "NSECTOR" ] = 24
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "JESUS MARIA" & datatable$SECTOR == "C" ), "NSECTOR" ] = 6
-
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "LINCE" & datatable$SECTOR == "A" ), "NSECTOR" ] = 54
-
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "MAGDALENA" & datatable$SECTOR == "A" ), "NSECTOR" ] = 34
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "MAGDALENA" & datatable$SECTOR == "C" ), "NSECTOR" ] = 30
-
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "PUEBLO LIBRE" & datatable$SECTOR == "A" ), "NSECTOR" ] = 22
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "PUEBLO LIBRE" & datatable$SECTOR == "B" ), "NSECTOR" ] = 17
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "PUEBLO LIBRE" & datatable$SECTOR == "C" ), "NSECTOR" ] = 13
-
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "SAN MIGUEL" & datatable$SECTOR == "A" ), "NSECTOR" ] = 19
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "SAN MIGUEL" & datatable$SECTOR == "B" ), "NSECTOR" ] = 19
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "SAN MIGUEL" & datatable$SECTOR == "C" ), "NSECTOR" ] = 44
-
-
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "SURQUILLO" & datatable$SECTOR == "A" ), "NSECTOR" ] = 13
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "SURQUILLO" & datatable$SECTOR == "B" ), "NSECTOR" ] = 6
 datatable[which(datatable$ESTRATO == "LIMA MODERNA" & datatable$DISTRITO == "SURQUILLO" & datatable$SECTOR == "C" ), "NSECTOR" ] = 28
-
-
-
 # DISTRITO LIMA CENTRO  
-
 datatable[which(datatable$ESTRATO == "LIMA CENTRO" & datatable$DISTRITO == "CERCADO LIMA" & datatable$SECTOR == "A" ), "NSECTOR" ] = 14
 datatable[which(datatable$ESTRATO == "LIMA CENTRO" & datatable$DISTRITO == "CERCADO LIMA" & datatable$SECTOR == "B" ), "NSECTOR" ] = 10
-
-
 datatable[which(datatable$ESTRATO == "LIMA CENTRO" & datatable$DISTRITO == "BRENIA" & datatable$SECTOR == "A" ), "NSECTOR" ] = 23
 datatable[which(datatable$ESTRATO == "LIMA CENTRO" & datatable$DISTRITO == "BRENIA" & datatable$SECTOR == "B" ), "NSECTOR" ] = 4
 datatable[which(datatable$ESTRATO == "LIMA CENTRO" & datatable$DISTRITO == "BRENIA" & datatable$SECTOR == "C" ), "NSECTOR" ] = 6
-
 datatable[which(datatable$ESTRATO == "LIMA CENTRO" & datatable$DISTRITO == "LA VICTORIA" & datatable$SECTOR == "A" ), "NSECTOR" ] = 37
-
 # san luis 0
-
-
 # DISTRITO LIMA ESTE 
-
 datatable[which(datatable$ESTRATO == "LIMA ESTE" & datatable$DISTRITO == "ATE" & datatable$SECTOR == "A" ), "NSECTOR" ] = 36
-
-# Cieneguilla
-# Chaclacayo
-# Lurigancho
-
+# Cieneguilla # Chaclacayo # Lurigancho
 datatable[which(datatable$ESTRATO == "LIMA ESTE" & datatable$DISTRITO == "SANTA ANITA" & datatable$SECTOR == "A" ), "NSECTOR" ] = 7
 datatable[which(datatable$ESTRATO == "LIMA ESTE" & datatable$DISTRITO == "EL AGUSTINO" & datatable$SECTOR == "A" ), "NSECTOR" ] = 3
 # San Juan de Lurigancho
-
 # DISTRITO NORTE 
-
 datatable[which(datatable$ESTRATO == "LIMA NORTE" & datatable$DISTRITO == "CARABAYLLO" & datatable$SECTOR == "A" ), "NSECTOR" ] = 23
 datatable[which(datatable$ESTRATO == "LIMA NORTE" & datatable$DISTRITO == "COMAS" & datatable$SECTOR == "A" ), "NSECTOR" ] = 42
-
 datatable[which(datatable$ESTRATO == "LIMA NORTE" & datatable$DISTRITO == "LOS OLIVOS" & datatable$SECTOR == "A" ), "NSECTOR" ] = 10
 datatable[which(datatable$ESTRATO == "LIMA NORTE" & datatable$DISTRITO == "LOS OLIVOS" & datatable$SECTOR == "B" ), "NSECTOR" ] = 44
 datatable[which(datatable$ESTRATO == "LIMA NORTE" & datatable$DISTRITO == "LOS OLIVOS" & datatable$SECTOR == "C" ), "NSECTOR" ] = 3
-
 # puente piedra
 datatable[which(datatable$ESTRATO == "LIMA NORTE" & datatable$DISTRITO == "SMP" & datatable$SECTOR == "A" ), "NSECTOR" ] = 6
 datatable[which(datatable$ESTRATO == "LIMA NORTE" & datatable$DISTRITO == "ANCON/INDEP" & datatable$SECTOR == "A" ), "NSECTOR" ] = 4
-
 # DISTRITO SUR 
-
-
 datatable[which(datatable$ESTRATO == "LIMA SUR" & datatable$DISTRITO == "CHORRILLOS" & datatable$SECTOR == "A" ), "NSECTOR" ] = 5
 datatable[which(datatable$ESTRATO == "LIMA SUR" & datatable$DISTRITO == "CHORRILLOS" & datatable$SECTOR == "C" ), "NSECTOR" ] = 30
-
 datatable[which(datatable$ESTRATO == "LIMA SUR" & datatable$DISTRITO == "LURIN/PACH/VES" & datatable$SECTOR == "A" ), "NSECTOR" ] = 13
 datatable[which(datatable$ESTRATO == "LIMA SUR" & datatable$DISTRITO == "SAN JUAN MIRAFLORES" & datatable$SECTOR == "A" ), "NSECTOR" ] = 11
 #Pucusana
 datatable[which(datatable$ESTRATO == "LIMA SUR" & datatable$DISTRITO == "PUNTA HERMOSA/NEGRA" & datatable$SECTOR == "A" ), "NSECTOR" ] = 13
 datatable[which(datatable$ESTRATO == "LIMA SUR" & datatable$DISTRITO == "SAN BARTOLO" & datatable$SECTOR == "A" ), "NSECTOR" ] = 14
-# Santa María del Mar
-
-
+# Santa Maria del Mar
 # DISTRITO CALLAO
-
 datatable[which(datatable$ESTRATO == "CALLAO" & datatable$DISTRITO == "BELLAVISTA" & datatable$SECTOR == "A" ), "NSECTOR" ] = 10
 datatable[which(datatable$ESTRATO == "CALLAO" & datatable$DISTRITO == "CALLAO" & datatable$SECTOR == "A" ), "NSECTOR" ] = 14
 datatable[which(datatable$ESTRATO == "CALLAO" & datatable$DISTRITO == "VENTANILLA" & datatable$SECTOR == "A" ), "NSECTOR" ] = 2
@@ -271,18 +202,12 @@ table(datatable$NESTRATO)
 table(datatable$NDISTRITO)
 table(datatable$NSECTOR)
 
-
-
-alpha = 0.05
-e = 0.05
-
-
 # W1 probabilidad de selecionar el estrato
 # W2 probabilidad de seleccionar el distrito dato que selecionamos el estrato
 # w3 probabilidad de seleccionar el sector dado que seleccionamos el distrito y el estrato
 
-
-NTotal_Estrato = sum(NESTRATO_LIMA_TOP, NESTRATO_LIMA_MODERNA, NESTRATO_LIMA_CENTRO, NESTRATO_LIMA_ESTE, NESTRATO_LIMA_NORTE, NESTRATO_LIMA_SUR, NESTRATO_CALLAO)
+NTotal_Estrato = sum(NESTRATO_LIMA_TOP, NESTRATO_LIMA_MODERNA, NESTRATO_LIMA_CENTRO, NESTRATO_LIMA_ESTE,
+                     NESTRATO_LIMA_NORTE, NESTRATO_LIMA_SUR, NESTRATO_CALLAO)
 
 datatable [ , FPC := NESTRATO ]
 datatable [ , FPC2 := NDISTRITO ]
@@ -294,50 +219,32 @@ datatable [ , WEIGHT := (WEIGHT_I * WEIGHT_J_I * WEIGHT_K_J_I)]
 
 
 # order by strato
-
-
 design = svydesign(id=~DISTRITO + SECTOR, fpc = ~FPC + FPC2 , strata = ~ESTRATO, data = datatable, weights = ~WEIGHT)
-
 design
-
 # estaimacion por metodo tradicional linealizacion
 options(survey.lonely.psu = "adjust")
-options(survey.lonely.psu = "certainty")
 mean = svymean(~BIM, design = design, deff = T)
 confint(mean)
-
-
-# estyimacion BRR 
+# estimacion JKN 
 jkn = as.svrepdesign(design=design, type="JKn")
 svymean(~BIM, design = jkn, deff = T)
-# bootstrap 
+# estimacion bootstrap 
 boot = as.svrepdesign(design=design, type="subbootstrap")
 svymean(~BIM, design = boot, deff = T)
 
+# PART B 
 
 top_sample = datatable[which(datatable$ESTRATO == "LIMA TOP" ),  ] 
 top_design = svydesign(id=~DISTRITO + SECTOR, fpc = ~FPC + FPC2 , data = top_sample, weights = ~WEIGHT)
 top_mean = svymean(~BIM, design = top_design, deff = T)
+top_mean
 confint(top_mean)
 
-# part c 
-
-datatable[ 1, "CONG"]
-datatable [ , nest := .N , by = 'ESTRATO']
-
-datatable [, Fact:=interaction(ESTRATO,DISTRITO, SECTOR)]
-
-datatable[ which(datatable$ESTRATO == "LIMA TOP"), "CONG" ] 
-
-# -------------------------    part c 
-
+# ---    part c 
 
 ppt_sample  = datatable[ which(datatable$ESTRATO == "LIMA TOP"), c("NUM","BIM","ESTRATO", "DISTRITO", "SECTOR", "CONG", "NDISTRITO", "NSECTOR") ] 
-
 ppt_sample [ , imputation := 0 ]
-
 # imputation miraflores c 133 obras en el distrito, 23 obras en sector C
-
 n_distrito_sector = 23
 prop <- rbeta(1, 2, 8)
 prop_yes <- ceiling(n_distrito_sector * prop)
@@ -348,7 +255,6 @@ for (index in rep(1,prop_yes)) {
 for (index in rep(1,prop_no)) {
   ppt_sample = rbind(ppt_sample, list( nrow(ppt_sample) + 1, "NO", "LIMA TOP", "MIRAFLORES", "C", "MIRAFLORESC", 133, n_distrito_sector, 1 ) )        
 }
-
 # imputation molina A 9 obras en el distrito, 6 obras en sector A
 n_distrito_sector = 6
 prop <- rbeta(1, 2, 8)
@@ -398,14 +304,8 @@ for (index in rep(1,prop_no)) {
 }
 
 ppt_sample$CONG = factor(ppt_sample$CONG)
-
 ppt_sample <- ppt_sample[order( ppt_sample$CONG ) , ]
-
 data.frame(table(ppt_sample$CONG))[ ,2 ]
-
-# MirafloresA, MirafloresB, MirafloresC, SanIsidroA, SanIsidroB, LaMolinaA, LaMolinaB, SantiagodeSurcoA, SantiagodeSurcoB, SantiagodeSurcoC, SanBorjaA, SanBorjaB, SanBorjaC, Barranco**(AyB)A
-
-
 DIS_SECTOR = sort(sapply(unique(ppt_sample$CONG), as.character))
 TAM_SECTOR = vector(length = length(DIS_SECTOR))
 BIM_SI_SECTOR = vector(length = length(DIS_SECTOR))
@@ -420,24 +320,48 @@ for (sector in DIS_SECTOR) {
   i = i + 1
 }
 
+# funcion hallar probabilidades de primer y segundo orden
+library(combinat)
+pisppt <- function(X,n) {
+  N = length(X)
+  XT = sum(X)
+  m = combn(X,n) # Requiere del paquete combinat
+  m = apply(m,2,permn)
+  m = matrix(unlist(m),ncol=n,byrow=TRUE)
+  nm = dim(m)[1]
+  p=0
+  for (j in 1:nm) {
+    p[j] = prod(m[j,])/(XT*prod(XT-cumsum(m[j,1:n-1])))
+  }
+  pi1=0
+  pi2=matrix(0,N,N)
+  for (i in 1:(N-1)){
+    aux1 = (m==X[i])
+    index = which(apply(1*aux1,1,sum)==1)
+    pi1[i] = sum(p[index])
+    for (j in (i+1):N){
+      aux2 = (m==X[j])
+      aux2 = 1*aux2[index,]
+      pi2[i,j] = sum(p[index[which(apply(aux2,1,sum)==1)]])
+    }}
+  pi1[N] = n-sum(pi1)
+  pi2 = pi2+t(pi2)
+  list(pi1,pi2)
+}
 
-
-
-
-
-PIK <- inclusionprobabilities(TAM_SECTOR, 4)
-PIK2 = UPsystematicpi2(PIK)
-ppt_obras =data.frame(DIS_SECTOR, TAM_SECTOR, BIM_SI_SECTOR, BIM_NO_SECTOR,  PROP_SECTOR, PIK)
-
-index = UPrandomsystematic(PIK)
-index = UPsampford(PIK)
+ppt_obras =data.frame(DIS_SECTOR, TAM_SECTOR, BIM_SI_SECTOR, BIM_NO_SECTOR,  PROP_SECTOR)
+probs = pisppt(as.numeric( TAM_SECTOR ),4)
+index = UPrandomsystematic(probs[[1]])
 ppt_obras_sample = getdata(ppt_obras, index)
+ppt_obras_pik = getdata(probs[[1]], as.logical(index))[, "data"]
 
-bim_result = HTestimator(ppt_obras_sample[,"BIM_SI_SECTOR"], ppt_obras_sample[,"PIK"]) 
+bim_result = HTestimator(ppt_obras_sample[,"PROP_SECTOR"], ppt_obras_pik) / 14
+bim_result
 
-pik2_index = PIK2[as.logical(index), as.logical(index)]
-# stimated error sqrt of variance
-sta =  sqrt(varHT(ppt_obras_sample[,"BIM_SI_SECTOR"] , pik2_index, 2))
+pik_2 =  probs[[2]][as.logical(index), as.logical(index)]
+diag(pik_2) = ppt_obras_pik 
+sta =  sqrt(varHT(ppt_obras_sample[,"PROP_SECTOR"] , pik_2, 1)) / 14
+
 
 e <- 0.05
 alpha = 0.05
@@ -445,6 +369,23 @@ z <- qnorm ( 1 - alpha / 2 )
 
 c(bim_result - z * sqrt( bim_result * (1 - bim_result) / 4 ), bim_result + z * sqrt(bim_result * (1-bim_result) / 4 ))
 
+
+
+PIK <- inclusionprobabilities(TAM_SECTOR, 4)
+p_1[[1]]
+
+
+sum(PIK) # 4
+
+
+
+PIK2 = UPsystematicpi2(PIK)
+index = UPrandomsystematic(PIK)
+ppt_obras_sample = getdata(ppt_obras, index)
+bim_result = HTestimator(ppt_obras_sample[,"PROP_SECTOR"], ppt_obras_sample[,"PIK"]) 
+pik2_index = PIK2[as.logical(index), as.logical(index)]
+# stimated error sqrt of variance
+sta =  sqrt(varHT(ppt_obras_sample[,"PROP_SECTOR"] , pik2_index, 2))
 
 
 
